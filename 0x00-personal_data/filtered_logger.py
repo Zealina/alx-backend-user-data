b064bbf84b9695e9554e08d60ac2d2e6981ed4d6
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-11 12:16:36 +0100

    Reduce func length

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,14 +9,6 @@ from typing import List
 def filter_datum(fields: List, redaction: str, message: str,
                  separator: str) -> str:
     """Return the log messages obfuscate
-    Args:
-        fields: a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character is
-        separating all fields in the log line (message)
-    Return:
-        the obfuscated log message
     """
     for field in fields:
         substitute = f'{field}={redaction};'
