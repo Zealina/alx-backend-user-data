c90c9fe5a660fc830e7630f8ca46083fae7907f3
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-15 18:31:24 +0100

    Create Basic Autg

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Authentification module for Basic Auth"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Contains all authentification parameters"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if authentification is required"""
+        if not path or not excluded_paths:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        return path in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """Checks the authorization header"""
+        if not request:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user"""
+        return None
