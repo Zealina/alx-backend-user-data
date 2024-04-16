d14428be4074c82b72c3b6672f04ab7d9453fdc0
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-16 07:18:58 +0100

    Create Basic Auth

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -7,11 +7,7 @@ from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
-def load_from_file_delayed():
-    """Delay the imports"""
-    from api.v1.views.index import *
-    from api.v1.views.users import *
+from api.v1.views.index import *
+from api.v1.views.users import *
 
-    User.load_from_file()
-
-load_from_file_delayed()
+User.load_from_file()
