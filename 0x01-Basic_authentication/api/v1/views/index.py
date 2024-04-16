610e4374a670cfc3a2400592efb117e1f001e112
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-16 07:44:38 +0100

    Proper Documentation

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,11 +7,13 @@ from api.v1.views import app_views
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
+    """Test the unauthorized end point"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> None:
+    """Test the forbidden endpoint"""
     abort(403)
 
 
