dd89238d55be851f7fd11e281321766227b5d9e6
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-16 08:01:34 +0100

    Create Auth Class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,16 +8,19 @@ from typing import List, TypeVar
 class Auth:
     """Contains all authentification parameters"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if authentification is required"""
+        """Checks if authentification is required
         if not path or not excluded_paths:
             return True
         if path[-1] != '/':
             path += '/'
-        return path in excluded_paths
+        return path in excluded_paths"""
+        return False
 
     def authorization_header(self, request=None) -> str:
         """Checks the authorization header"""
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns the current user"""
