0efe9be2875d7ca34cc314774b51d344ec09c0bc
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-16 14:27:47 +0100

    Base64 Decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Create Basic Authentication"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,19 @@ class BasicAuth(Auth):
         if split[0] != 'Basic':
             return
         return split[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """decode base64 authorization header"""
+        if not base64_authorization_header:
+            return
+        if type(base64_authorization_header) is not str:
+            return
+        try:
+            header = base64_authorization_header
+            header = base64.b64decode(header)
+            header = header.decode('utf-8')
+        except base64.binascii.Error:
+            return
+        return header
