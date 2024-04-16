60f076988b2052da3a1b2381fb52e5bfca3416eb
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-16 14:12:38 +0100

    Extract encoded password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic  authentication class """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extract base64 authorization header"""
+        if not authorization_header:
+            return
+        if type(authorization_header) is not str:
+            return
+        split = authorization_header.split()
+        if split[0] != 'Basic':
+            return
+        return split[1]
