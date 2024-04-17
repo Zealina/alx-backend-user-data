c1ae73469de168e903c078841e89561c2e0897db
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-17 22:25:14 +0100

    In memory session store

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,17 @@
 """Session based Auth module"""
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Authentification class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create all parameters for a session"""
+        if not user_id or type(user_id) is not str:
+            return
+        sess_id = str(uuid4())
+        self.__class__.user_id_by_session_id[sess_id] = user_id
+        return sess_id
