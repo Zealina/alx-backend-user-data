e7e972d1a892b9b8e43403f16fa26917bbe06ab6
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-18 00:01:58 +0100

    Create Nothing

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,11 +32,15 @@ def before_request() -> None:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.authorization_header(request):
+        if auth.session_cookie(request):
+            return None, abort(401)
     user = auth.current_user(request)
     if user is None:
         abort(403)
