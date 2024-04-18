50209002a42fea10792a6561ad8438e9521e6111
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-18 07:52:01 +0100

    Session Auth Before Request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,11 +36,10 @@ def before_request() -> None:
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
+        print(auth.session_cookie(request))
         abort(401)
-    if auth.authorization_header(request):
-        if auth.session_cookie(request):
-            return None, abort(401)
     user = auth.current_user(request)
     if user is None:
         abort(403)
