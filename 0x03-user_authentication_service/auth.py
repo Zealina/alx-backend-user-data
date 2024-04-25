b016e0de429349ed5b86f093febeeeb602ea9da4
Zealina Hudson <zealinahudson@gmail.com>
2024-04-25 16:37:35 +0100

    Change environments

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Auth module"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password and return bytes"""
+    salt = bcrypt.gensalt
+    return bcrypt.hashpw(password, salt)
