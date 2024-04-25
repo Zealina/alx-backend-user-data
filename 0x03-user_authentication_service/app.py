72698570861378a3547ff72fe92706c755046611
Zealina Hudson <zealinahudson@gmail.com>
2024-04-25 23:25:00 +0100

    Hopium

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,5 +68,16 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.rouute("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """Reset password for user"""
+    email = request.cookies.get("email")
+    try:
+        token = AUTH.get_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError as e:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
