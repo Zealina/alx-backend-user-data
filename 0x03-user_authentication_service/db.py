3412f7b8051e61204eee4620d25bc0046411402e
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-25 14:45:16 +0000

    Raise Errors

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,15 @@ class DB:
         self._session.add(new)
         self._session.commit()
         return new
+
+    def find_user_by(self, **kwargs):
+        """Get first user matching  keyword"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except NoResultFound as e:
+            raise NoResultFound
+        except InvalidRequestError as e:
+            raise InvalidRequestError
