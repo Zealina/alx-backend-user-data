b9a82b0dd0a2566a3a9179a88e329977cb2d0d39
Zealina Hudson <zealinahudson@gmail.com>
2024-04-25 17:51:04 +0100

    Create Post end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Flask app"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -11,6 +13,20 @@ app = Flask(__name__)
 def index():
     """Index page return"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """Create user if not exists"""
+    email = request.form["email"]
+    password = request.form["password"]
+    if not email or not password:
+        abort(400)
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
