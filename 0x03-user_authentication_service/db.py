96cfeca0b90a4668b39cae81030bc9805b7fbcc6
Zealina Hudson <zealinahudson@gmail.com>
2024-04-25 16:28:11 +0100

    Update Uset

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,3 +55,12 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update User stuffy stuff stuff"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
