1963b41a430326b8694a61555f85f183e92a1046
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-25 21:18:58 +0000

    Delete session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,12 +50,12 @@ def logout():
     """Destroy a session"""
     session_id = request.cookies.get("session_id")
     if not session_id:
-        abort(400)
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect(url_for('index'))
+    return redirect('/')
 
 
 if __name__ == "__main__":
