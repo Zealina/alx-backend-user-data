c592a95c6ef8ede75a257d390f2f6b29bf9a784c
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-25 20:26:17 +0000

    Login and stuff

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -27,6 +27,21 @@ def register_user():
         return jsonify({"email": email, "message": "user created"})
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def create_sessh():
+    """Create new session on login"""
+    email = request.form['email']
+    password = request.form['password']
+    if not email or not password:
+        abort(400)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    out = jsonify({"email": email, "message": "logged in"})
+    out.set_cookie("session_id", session_id)
+    return out
 
 
 if __name__ == "__main__":
