8f720e7ff9abe2d224903bd6dea3078b1e5c937a
Zealina Hudson <zealinahudson@gmail.com>
2024-04-25 16:44:21 +0100

    Encode and hash

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,5 +6,5 @@ import bcrypt
 
 def _hash_password(password: str) -> bytes:
     """Hash a password and return bytes"""
-    salt = bcrypt.gensalt
-    return bcrypt.hashpw(password, salt)
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode("utf-8"), salt)
